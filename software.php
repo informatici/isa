d16ea5d43a84f1ccc3227c796c53e2aa22776622
Roberto De Nicolò <rodenic@gmail.com>
2013-01-14 23:53:33 +0100

    Aggiunto loader e predisposti loghi per la sezione software

--- software.php
+++ software.php
@@ -50,12 +50,14 @@ include "functions.php";
 
 	<? include "leftcolumn.php"; ?>
 
-	<a class="software" href="http://cameramouse.org/downloads.html">CameraMouse</a>
-	<a class="software" href="http://www.gazegroup.org/downloads/23-gazetracker">Gaze tracker</a>
-	<a class="software" href="http://sourceforge.net/projects/eviacam/files/">eViacam</a>
-	<a class="software" href="http://robotica.udl.cat/">HeadMouse</a>
-	<a class="software" href="http://myeye.jimdo.com/download/">myEye</a>
-	<a class="credits" href="http://www.informaticisenzafrontiere.org/2012/09/i-s-a-i-speak-again/">credits</a>
+	<div id="software">
+		<a href="http://cameramouse.org/downloads.html"><span>CameraMouse</span></a>
+		<a href="http://www.gazegroup.org/downloads/23-gazetracker"><span>Gaze tracker</span></a>
+		<a href="http://sourceforge.net/projects/eviacam/files/"><span>eViacam</span></a>
+		<a href="http://robotica.udl.cat/"><span>HeadMouse</span></a>
+		<a href="http://myeye.jimdo.com/download/"><span>myEye</span></a>
+		<a href="http://www.informaticisenzafrontiere.org/2012/09/i-s-a-i-speak-again/"><span>credits</span></a>
+	</div>
 
 </div>
 
