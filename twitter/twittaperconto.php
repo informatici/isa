efd14fbb060308831e8e7226075947fc4c4a0595
Roberto De Nicolò <rodenic@gmail.com>
2013-01-14 08:13:48 +0100

    Modifiche minori

---
+++ twitter/twittaperconto.php
@@ -0,0 +1,1 @@
+<iframe height="100" width="400" frameborder="0" scrolling="no" allowtransparency="true" id="twitter_wall" name="twitter_wall" src="http://www.walks.to/isa/twitter/index_embed.php?id=araihc"</iframe>
